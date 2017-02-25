a41c9977db38a757ee6df948f2dc5ddef4ab9fdf
Benoit Lemoine <blemoine@mnubo.com>
2017-02-25 15:45:54 -0500

    feat(vue): init vue

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: { cv: './src/cv.ts' },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js' //Temporary, must precompile templates
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+};
